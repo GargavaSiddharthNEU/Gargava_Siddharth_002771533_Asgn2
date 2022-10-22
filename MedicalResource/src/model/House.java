cfd3c6c8c54ba108513ea8e6323755beded10f72
GargavaSiddharthNEU <gargava.s@northeastern.edu>
2022-10-22 13:47:59 -0400

    22nd Oct 1:47pm

---
+++ MedicalResource/src/model/House.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author siddh
+ */
+public class House {
+    private String id;
+    private String houseNumber;
+    private String street;
+    private String city;
+    private String country;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
--- MedicalResource/src/model/Person.java
+++ MedicalResource/src/model/Person.java
@@ -9,5 +9,85 @@ package model;
  * @author siddh
  */
 public class Person {
-    
+    private String id;
+    private String ssn;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String phoneNumber;
+    private House house;
+    private Boolean isPatient;
+    private Boolean isDoctor;
+
+    public Boolean getIsDoctor() {
+        return isDoctor;
+    }
+
+    public void setIsDoctor(Boolean isDoctor) {
+        this.isDoctor = isDoctor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public Boolean getIsPatient() {
+        return isPatient;
+    }
+
+    public void setIsPatient(Boolean isPatient) {
+        this.isPatient = isPatient;
+    }
 }
